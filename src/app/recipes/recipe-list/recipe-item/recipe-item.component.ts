5088619e4c4f5a823a7c41c8fd1d3675e388e097
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-12 17:41:17 -0400

    Add output event emitter

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 Recipe;
 @Component({
@@ -9,6 +9,12 @@ Recipe;
 export class RecipeItemComponent implements OnInit {
   @Input()
   recipe: Recipe;
+  @Output()
+  recipeSelected = new EventEmitter<void>();
+
+  onSelected() {
+    this.recipeSelected.emit();
+  }
   constructor() {}
 
   ngOnInit() {}
