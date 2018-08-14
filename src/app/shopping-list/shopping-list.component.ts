4710f5f66df7e67b8830a4b3f60253c9f18bf620
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-13 21:00:21 -0400

    Add functionality to add ingredient to ingredients array

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -13,5 +13,8 @@ export class ShoppingListComponent implements OnInit {
   ];
   constructor() {}
 
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
   ngOnInit() {}
 }
