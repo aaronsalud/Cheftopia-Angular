8e571061ff053d417e94c6476592d801348ccce6
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-13 22:29:12 -0400

    Add RecipeService

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,21 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Chicken Inasal',
+      'This is the recipe',
+      'https://images.summitmedia-digital.com/yummyph/images/04-2013_recipes/04-2013_yummy-ph_recipe_image_sari-jorges-chicken-inasal_main.jpg'
+    ),
+    new Recipe(
+      'Chicken Tikka',
+      'This is the recipe',
+      'https://images.summitmedia-digital.com/yummyph/images/04-2013_recipes/04-2013_yummy-ph_recipe_image_sari-jorges-chicken-inasal_main.jpg'
+    )
+  ];
+
+  getRecipes() {
+    // Return as a new Recipe array
+    return this.recipes.slice();
+  }
+}
