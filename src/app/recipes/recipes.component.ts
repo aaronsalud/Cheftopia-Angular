9fa16839a74e2c3d17263c82245f3aa32a2b52bd
Aaron Mikhail Salud <amsalud@quartermaster.house>
2018-08-14 16:39:46 -0400

    Cleanup Recipes component

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -9,18 +9,11 @@ import { RecipeService } from './recipe.service';
   providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
-  recipes: Recipe[];
-  selectedRecipe: Recipe;
-  constructor(private recipeService: RecipeService) {}
+
+  constructor(private recipeService: RecipeService) {
+  
+  }
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => (this.selectedRecipe = recipe)
-    );
-    this.recipes = this.recipeService.getRecipes();
-
-    if (!this.selectedRecipe) {
-      this.selectedRecipe = this.recipes[0];
-    }
   }
 }
