cba7c0fe2ce1d334776b9ebd86139d82e6bdd8ce
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-14 21:07:12 -0400

    Add ingredients sample data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -8,12 +9,14 @@ export class RecipeService {
     new Recipe(
       'Chicken Inasal',
       'This is the recipe',
-      'https://images.summitmedia-digital.com/yummyph/images/04-2013_recipes/04-2013_yummy-ph_recipe_image_sari-jorges-chicken-inasal_main.jpg'
+      'https://images.summitmedia-digital.com/yummyph/images/04-2013_recipes/04-2013_yummy-ph_recipe_image_sari-jorges-chicken-inasal_main.jpg',
+      [new Ingredient('Chicken', 1), new Ingredient('Garlic', 20)]
     ),
     new Recipe(
       'Chicken Tikka',
       'This is the recipe',
-      'http://assets.kraftfoods.com/recipe_images/opendeploy/173356_640x428.jpg'
+      'http://assets.kraftfoods.com/recipe_images/opendeploy/173356_640x428.jpg',
+      [new Ingredient('Chicken', 2), new Ingredient('Pepper', 20)]
     )
   ];
 
@@ -22,8 +25,7 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
-  setActiveRecipeItem(recipe :Recipe){
+  setActiveRecipeItem(recipe: Recipe) {
     this.activeRecipeItem = recipe;
   }
-
 }
