5318789a22241d06705f9bc42cb86168dca4c4e3
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-15 22:22:43 -0400

    Add new routes for recipe edit and creation

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -2,12 +2,15 @@ import { Routes } from '@angular/router';
 import { RecipesComponent } from '../components/recipes/recipes.component';
 import { ShoppingListComponent } from '../components/shopping-list/shopping-list.component';
 import { RecipeDetailComponent } from '../components/recipes/recipe-detail/recipe-detail.component';
+import { RecipeFormComponent } from '../components/recipes/recipe-form/recipe-form.component';
 
 export const appRoutes: Routes = [
   {
     path: 'recipes', component: RecipesComponent, children: [
-      { path: ':id', component: RecipeDetailComponent },
-      { path: '', component: RecipeDetailComponent }
+      { path: '', component: RecipeDetailComponent },
+      { path: 'new', component: RecipeFormComponent },
+      { path: ':id', component: RecipeDetailComponent },  
+      { path: ':id/edit', component: RecipeFormComponent }, 
     ]
   },
   { path: 'shopping-list', pathMatch: 'full', component: ShoppingListComponent },
