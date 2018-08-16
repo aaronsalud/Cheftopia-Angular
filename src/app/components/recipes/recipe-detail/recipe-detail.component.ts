ec57c228a10b9a1c2d319df1bbeaf71ef4ceb3d5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-15 22:45:38 -0400

    Add method to redirect to recipes/:id/edit

--- src/app/components/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { Ingredient } from '../../shared/ingredient.model';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 
 @Component({
@@ -14,10 +14,14 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) { }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.recipeService.addIngredientsToShoppingList(ingredients);
+  }
+
+  editRecipe(){
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 
   ngOnInit() {
