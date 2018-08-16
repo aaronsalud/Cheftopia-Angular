4c3f078bfa116dbbb5f504cd92f9a0b64f0cf416
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-16 17:14:58 -0400

    Add method to initialize form

--- src/app/components/recipes/recipe-form/recipe-form.component.ts
+++ src/app/components/recipes/recipe-form/recipe-form.component.ts
@@ -1,25 +1,47 @@
-import { Component, OnInit } from "@angular/core";
-import { RecipeService } from "../recipe.service";
-import { ActivatedRoute, Params } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-    selector: 'app-recipe-form',
-    templateUrl: './recipe-form.component.html',
-    styleUrls: ['./recipe-form.component.css'],
-    providers: [RecipeService]
-  })
-  export class RecipeFormComponent implements OnInit {
-    id: number;
-    editMode = false;
-    constructor(private route: ActivatedRoute) {}
-  
-    ngOnInit() {
-      this.route.params.subscribe((params: Params)=>{
-        this.id = +params['id'];
-        // Toggle edit mode if the id is set
-        this.editMode = params['id'] != null;
+  selector: 'app-recipe-form',
+  templateUrl: './recipe-form.component.html',
+  styleUrls: ['./recipe-form.component.css'],
+  providers: [RecipeService]
+})
+export class RecipeFormComponent implements OnInit {
+  id: number;
+  editMode = false;
+  recipeForm: FormGroup;
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
 
-      })
-    }
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      // Toggle edit mode if the id is set
+      this.editMode = params['id'] != null;
+      this.initForm();
+    });
   }
-  
+
+  private initForm() {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipeByIndex(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName),
+      imagePath: new FormControl(recipeImagePath),
+      description: new FormControl(recipeDescription)
+    });
+  }
+}
