1608da9709dcded8503a8cfaacea59cf3c4f4b3a
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-15 21:51:56 -0400

    Add child routes to the recipes

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,13 +1,20 @@
 import { Routes } from '@angular/router';
 import { RecipesComponent } from '../components/recipes/recipes.component';
-import { ShoppingListComponent} from '../components/shopping-list/shopping-list.component';
+import { ShoppingListComponent } from '../components/shopping-list/shopping-list.component';
+import { RecipeDetailComponent } from '../components/recipes/recipe-detail/recipe-detail.component';
 
 export const appRoutes: Routes = [
-    { path: 'recipes', pathMatch: 'full', component: RecipesComponent },
-    { path: 'shopping-list', pathMatch: 'full', component: ShoppingListComponent },
-    { path: '',
-      redirectTo: '/recipes',
-      pathMatch: 'full'
-    },
-    // { path: '**', component: PageNotFoundComponent }
+  {
+    path: 'recipes', component: RecipesComponent, children: [
+      { path: ':id', component: RecipeDetailComponent },
+      { path: '', component: RecipeDetailComponent }
+    ]
+  },
+  { path: 'shopping-list', pathMatch: 'full', component: ShoppingListComponent },
+  {
+    path: '',
+    redirectTo: '/recipes',
+    pathMatch: 'full'
+  },
+  // { path: '**', component: PageNotFoundComponent }
 ];
