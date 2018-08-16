11408208d1b5656ac356f57fe1650a9c40f2d403
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-16 16:52:13 -0400

    Implement method to delete Ingredient by Index

--- src/app/components/shopping-list/shopping-list.service.ts
+++ src/app/components/shopping-list/shopping-list.service.ts
@@ -36,4 +36,9 @@ export class ShoppingListService {
     this.ingredients[index] = newIngredient;
     this.ingredientsChanged.next(this.ingredients.slice());
   }
+
+  deleteIngredientByIndex(index: number) {
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
 }
