d36124907799212bdbac7bd1db487aaa4ae709d1
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-16 16:09:56 -0400

    Implement subscribe and unsubscribe method to get the Ingredient item index to be edited

--- src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  private ingredientEditSubscription: Subscription;
+  editedItemIndex: number;
+  editMode = false;
+
   constructor(private shoppingListService: ShoppingListService) {}
 
   saveItem(form: NgForm) {
@@ -19,5 +24,16 @@ export class ShoppingEditComponent implements OnInit {
     }
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.ingredientEditSubscription = this.shoppingListService.ingredientEdit.subscribe(
+      (index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.ingredientEditSubscription.unsubscribe();
+  }
 }
