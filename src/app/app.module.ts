a0f555e49f3f9430dbf3e87fd70aa484de9594e8
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-16 15:33:09 -0400

    Import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
 import { RecipeListComponent } from './components/recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './components/recipes/recipe-detail/recipe-detail.component';
+import { RecipeFormComponent } from './components/recipes/recipe-form/recipe-form.component';
 import { RecipeItemComponent } from './components/recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './components/shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListService } from './components/shopping-list/shopping-list.service';
-import { AppRoutingModule } from './routes/app-routing.module';
-import { RecipeFormComponent } from './components/recipes/recipe-form/recipe-form.component';
 
 @NgModule({
   declarations: [
@@ -27,8 +27,8 @@ import { RecipeFormComponent } from './components/recipes/recipe-form/recipe-for
     ShoppingListComponent,
     ShoppingEditComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, NgbModule.forRoot(), ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, NgbModule.forRoot()],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
