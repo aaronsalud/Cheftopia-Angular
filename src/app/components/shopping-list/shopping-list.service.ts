10b414e7a99d03d7e224e3a6b7f5835eff70a632
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-16 16:08:23 -0400

    Implement Observables instead of EventEmitter

--- src/app/components/shopping-list/shopping-list.service.ts
+++ src/app/components/shopping-list/shopping-list.service.ts
@@ -1,12 +1,13 @@
 import { Ingredient } from '../shared/ingredient.model';
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class ShoppingListService {
-
-  constructor(private router: Router) { }
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  constructor(private router: Router) {}
+  ingredientsChanged = new Subject<Ingredient[]>();
+  ingredientEdit = new Subject<number>();
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
@@ -14,13 +15,13 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
     this.router.navigate(['/shopping-list']);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   getIngredients() {
