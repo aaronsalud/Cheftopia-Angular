5ceda2f6008c6fbe746ebb55417ab324ece9f9f5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-16 17:22:20 -0400

    Implement onSubmit method

--- src/app/components/recipes/recipe-form/recipe-form.component.ts
+++ src/app/components/recipes/recipe-form/recipe-form.component.ts
@@ -27,6 +27,10 @@ export class RecipeFormComponent implements OnInit {
     });
   }
 
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
