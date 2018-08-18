010727cdb740e3b8b6a853433aff404cc0d0dc09
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-17 22:10:07 -0400

    Create Profile model

---
+++ models/profile.js
@@ -0,0 +1,25 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Profile = sequelize.define(
+    'Profile',
+    {
+      website: DataTypes.STRING,
+      bio: DataTypes.STRING,
+      restaurant: DataTypes.STRING,
+      location: DataTypes.STRING,
+      occupation: DataTypes.STRING,
+      user_id: DataTypes.INTEGER
+    },
+    {
+      timestamps: true,
+      underscored: true,
+      tableName: 'profiles'
+    }
+  );
+  Profile.associate = function(models) {
+    // associations can be defined here
+    const { User } = models;
+    Profile.belongsTo(User);
+  };
+  return Profile;
+};
