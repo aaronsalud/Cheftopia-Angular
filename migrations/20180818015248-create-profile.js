97703b4760cfb5f91b1573fcea140c28ebe7b802
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-17 22:09:49 -0400

    Create migration to create profile table

---
+++ migrations/20180818015248-create-profile.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      website: {
+        type: Sequelize.STRING
+      },
+      bio: {
+        type: Sequelize.STRING
+      },
+      restaurant: {
+        type: Sequelize.STRING
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      occupation: {
+        type: Sequelize.STRING
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE(3),
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3)')
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE(3),
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3)')
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('profiles');
+  }
+};
