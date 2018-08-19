2a86d02a5fef4a2bed790b81c26ff0a913eea262
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-18 23:30:42 -0400

    Fix drop statement

--- migrations/20180818231136-create-profile-recipe.js
+++ migrations/20180818231136-create-profile-recipe.js
@@ -27,6 +27,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('ProfileRecipes');
+    return queryInterface.dropTable('profile_recipe');
   }
 };
