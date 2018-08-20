a621b9d10300949c72114e8854e7215b2c167a7d
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-19 21:04:38 -0400

    Add new route to update a shopping list

--- routes/api/shoppinglist.js
+++ routes/api/shoppinglist.js
@@ -2,16 +2,14 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-// Load Profile Model
-const {
-  Profile,
-  User,
-  ShoppingList,
-  UserShoppingList
-} = require('../../models');
+// Load Models
+const { User, ShoppingList } = require('../../models');
+
+//Load Input Validators
+const validateShoppingListInput = require('../../validators/shoppinglist');
 
 // @route GET api/shoppinglist
-// @desc Get Current User shoppinglist
+// @desc Get Current User shopping list
 // @params { archived: 1 | 0 }
 // @access Private
 router.get(
@@ -40,6 +38,99 @@ router.get(
           throw { error: 'User not found' };
         }
         res.json(user.shopping_lists);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route POST api/shoppinglist
+// @desc Create a Shopping List
+// @access Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateShoppingListInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    User.findOne({
+      where: {
+        id: req.user.id
+      }
+    })
+      .then(user => {
+        const fields = ['name', 'description'];
+        let newShoppingList = {};
+
+        fields.forEach(field => {
+          if (req && req.body && req.body[field]) {
+            newShoppingList[field] = req.body[field];
+          }
+        });
+
+        // Create new shopping list
+        ShoppingList.create(newShoppingList)
+          .then(createdShoppingList => {
+            // Add shoppinglist to pivot with user
+            user
+              .addShopping_list(createdShoppingList.id)
+              .then(response => res.json(createdShoppingList))
+              .catch(err => res.status(404).json(err));
+          })
+          .catch(err =>
+            res.status(404).json({ error: 'Failed to create new recipe' })
+          );
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route PUT api/shoppinglist/:id
+// @desc Update a Shopping List
+// @access Private
+router.put(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateShoppingListInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    User.findOne({
+      where: {
+        id: req.user.id
+      }
+    })
+      .then(user => {
+        const fields = ['name', 'description', 'archived'];
+        let newShoppingList = {};
+
+        fields.forEach(field => {
+          if (req && req.body && req.body[field]) {
+            newShoppingList[field] = req.body[field];
+          }
+        });
+
+        // Update shopping list
+        ShoppingList.update(newShoppingList, {
+          returning: true,
+          where: { id: req.params.id }
+        })
+          .then(([rowsUpdated, [updatedShoppingList]]) => {
+            res.json(updatedShoppingList);
+          })
+          .catch(err =>
+            res.status(404).json({ error: 'Failed to update shopping list' })
+          );
       })
       .catch(err => res.status(404).json(err));
   }
