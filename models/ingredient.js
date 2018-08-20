59341b6cc625e60d63a9b435b3b75497387b1d24
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-20 17:33:53 -0400

    Define relationship to many recipes

--- models/ingredient.js
+++ models/ingredient.js
@@ -10,13 +10,20 @@ module.exports = (sequelize, DataTypes) => {
     });
   Ingredient.associate = function (models) {
     // associations can be defined here
-    const { ShoppingList, ShoppingListIngredient } = models;
+    const { ShoppingList, ShoppingListIngredient, RecipeIngredient, Recipe } = models;
 
     ShoppingList.belongsToMany(Ingredient, {
       as: 'shopping_lists',
       through: ShoppingListIngredient,
       foreignKey: 'ingredient_id',
       otherKey: 'shoppinglist_id'
+    });
+
+    Ingredient.belongsToMany(Recipe, {
+      as: 'recipes',
+      through: RecipeIngredient,
+      foreignKey: 'ingredient_id',
+      otherKey: 'recipe_id'
     });
   };
   return Ingredient;
