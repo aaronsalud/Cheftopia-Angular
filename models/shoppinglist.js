f98c98640115f44fdaa264157b1a8cf35f84f5d5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-19 20:26:33 -0400

    Add association to many users

--- models/shoppinglist.js
+++ models/shoppinglist.js
@@ -1,14 +1,26 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var ShoppingList = sequelize.define('ShoppingList', {
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    archived: DataTypes.BOOLEAN
-  }, {
-    underscored: true,
-  });
+  var ShoppingList = sequelize.define(
+    'ShoppingList',
+    {
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      archived: DataTypes.BOOLEAN
+    },
+    {
+      underscored: true,
+      tableName: 'shopping_lists'
+    }
+  );
   ShoppingList.associate = function(models) {
     // associations can be defined here
+    const { User, UserShoppingList } = models;
+    ShoppingList.belongsToMany(User, {
+      as: 'users',
+      through: UserShoppingList,
+      foreignKey: 'shoppinglist_id',
+      otherKey: 'user_id'
+    });
   };
   return ShoppingList;
 };
