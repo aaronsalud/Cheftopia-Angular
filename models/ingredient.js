c4651d4898981c1a3ee706b0968eb11c4e233318
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-22 16:49:08 -0400

    Add ingredientable and ingredientable_id to model

--- models/ingredient.js
+++ models/ingredient.js
@@ -2,7 +2,9 @@
 module.exports = (sequelize, DataTypes) => {
   var Ingredient = sequelize.define('Ingredient', {
     name: DataTypes.STRING,
-    amount: DataTypes.INTEGER
+    amount: DataTypes.INTEGER,
+    ingredientable: DataTypes.STRING,
+    ingredientable_id: DataTypes.INTEGER
   }, {
       timestamps: true,
       underscored: true,
