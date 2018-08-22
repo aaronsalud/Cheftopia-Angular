432606b85bc9394546bd00b4c95ae726ff169aa1
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-21 21:38:32 -0400

    Add Shoppinglist model options

--- helpers/model-options.js
+++ helpers/model-options.js
@@ -1,25 +1,31 @@
 // Load Models
-const {  User, Recipe, Ingredient } = require('../models');
+const { User, Recipe, Ingredient, ShoppingList } = require('../models');
 const modelOptions = {
-    user: {
-        model: User,
-        as: 'user',
-        required: true,
-        attributes: ['id', 'name', 'avatar']
-    },
-    recipes: {
-        model: Recipe,
-        as: 'recipes',
-        required: false,
-        through: { attributes: [] }
-    },
+  user: {
+    model: User,
+    as: 'user',
+    required: true,
+    attributes: ['id', 'name', 'avatar']
+  },
+  recipes: {
+    model: Recipe,
+    as: 'recipes',
+    required: false,
+    through: { attributes: [] }
+  },
 
-    ingredients: {
-        model: Ingredient,
-        as: 'ingredients',
-        attributes: { exclude: ['ingredientable', 'ingredientable_id'] },
-        required: false
-    }
-}
+  ingredients: {
+    model: Ingredient,
+    as: 'ingredients',
+    attributes: { exclude: ['ingredientable', 'ingredientable_id'] },
+    required: false
+  },
+  shoppinglist: {
+    model: ShoppingList,
+    as: 'shopping_lists',
+    through: { attributes: [] },
+    required: false
+  }
+};
 
 module.exports = modelOptions;
