5fe7a92b00f8ef1c71144bc1c5b9b87e0f7cd155
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-23 11:31:04 -0400

    Validate shoppinglist description

--- validators/shoppinglist.js
+++ validators/shoppinglist.js
@@ -3,7 +3,7 @@ const isEmpty = require('./is-empty');
 module.exports = function validateShoppingListInput(data) {
   let errors = {};
 
-  const key_attributes = ['name'];
+  const key_attributes = ['name', 'description'];
 
   key_attributes.forEach(key => {
     data[key] = !isEmpty(data[key]) ? data[key] : '';
@@ -11,6 +11,9 @@ module.exports = function validateShoppingListInput(data) {
 
   if (Validator.isEmpty(data.name)) {
     errors.name = 'Name field is required';
+  }
+  if (Validator.isEmpty(data.description)) {
+    errors.description = 'Description field is required';
   }
 
   return {
