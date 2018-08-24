fbca016a7f0a16a98466105904da014b22cf2a15
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-24 15:15:07 -0400

    Re implement method to update recipe with ingredients

--- routes/api/recipe.js
+++ routes/api/recipe.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-const getValuesByKey = require('../../helpers/get-values-by-key');
 // Load Models
-const { Profile, User, Recipe, Ingredient } = require('../../models');
+const { Profile, Recipe, Ingredient } = require('../../models');
 
 // Load model loading options
 const modelOptions = require('../../helpers/model-options');
@@ -87,7 +86,7 @@ router.post(
 // @desc  Update a recipe
 // @access Private
 router.put(
-    '/recipe/:id',
+    '/:id',
     passport.authenticate('jwt', { session: false }),
     (req, res) => {
         const { errors, isValid } = validateRecipeInput(req.body);
@@ -98,39 +97,46 @@ router.put(
             return res.status(400).json(errors);
         }
 
-        // Ensure that the only the user currently logged in can edit the recipe
-        Profile.findOne({
-            where: { user_id: req.user.id }
-        })
-            .then(profile => {
-                const fields = ['name', 'image', 'description', 'ingredients'];
-                let newRecipe = {};
+        const fields = ['name', 'image', 'description', 'ingredients'];
+        let newRecipe = { user_id: req.user.id };
 
-                fields.forEach(field => {
-                    if (req && req.body && req.body[field]) {
-                        newRecipe[field] = req.body[field];
-                    }
-                });
+        fields.forEach(field => {
+            if (req && req.body && req.body[field]) {
+                newRecipe[field] = req.body[field];
+            }
+        });
 
-                // Update recipe
-                Recipe.update(newRecipe, {
-                    returning: true,
-                    where: { id: req.params.id }
-                })
-                    .then(([rowsUpdated, [updatedRecipe]]) => {
-                        if (updatedRecipe) {
-                            res.json(updatedRecipe);
-                        } else {
-                            throw { error: 'Recipe not found' };
+        Recipe.findById(req.params.id)
+            .then(recipe => {
+                if (!recipe) {
+                    throw { error: 'Recipe not found' }
+                }
+                // Destroy
+                Ingredient.destroy({ where: { ingredientable_id: req.params.id } })
+                    .then(response => response)
+                    .then(response => {
+                        // If there are no ingredients to be updated return the recipe data
+                        if (!newRecipe.ingredients || newRecipe.ingredients.length == 0) {
+                            return Recipe.findById(req.params.id, { include: [modelOptions.ingredients] }).then(recipe => res.json(recipe));
                         }
+
+                        const newIngredients = newRecipe.ingredients.slice();
+
+                        newIngredients.map(ingredient => {
+                            ingredient.ingredientable_id = req.params.id;
+                            ingredient.ingredientable = 'Recipe';
+                        });
+
+                        // Create the ingredients
+                        Ingredient.bulkCreate(newIngredients, { returning: true }).then(createdIngredients => {
+                            return createdIngredients;
+                        }).then(response => {
+                            // Return the newly created recipe with the ingredients
+                            Recipe.findById(req.params.id, { include: [modelOptions.ingredients] }).then(recipe => res.json(recipe));
+                        });
                     })
-                    .catch(err => res.status(404).json(err));
             })
-            .catch(err =>
-                res
-                    .status(401)
-                    .json({ error: 'User is unauthorized to perform this action' })
-            );
+            .catch(err => res.status(404).json(err))
     }
 );
 
