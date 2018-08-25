5748eafa5d68551f0ee71c2fe99c8415073ceb3e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-25 18:00:53 -0400

    Map default route to the landing page

--- ui/src/app/routes/app.routes.ts
+++ ui/src/app/routes/app.routes.ts
@@ -3,21 +3,29 @@ import { RecipesComponent } from '../components/recipes/recipes.component';
 import { ShoppingListComponent } from '../components/shopping-list/shopping-list.component';
 import { RecipeDetailComponent } from '../components/recipes/recipe-detail/recipe-detail.component';
 import { RecipeFormComponent } from '../components/recipes/recipe-form/recipe-form.component';
+import { LandingComponent } from '../components/landing/landing.component';
 
 export const appRoutes: Routes = [
   {
-    path: 'recipes', component: RecipesComponent, children: [
+    path: 'recipes',
+    component: RecipesComponent,
+    children: [
       { path: '', component: RecipeDetailComponent },
       { path: 'new', component: RecipeFormComponent },
-      { path: ':id', component: RecipeDetailComponent },  
-      { path: ':id/edit', component: RecipeFormComponent }, 
+      { path: ':id', component: RecipeDetailComponent },
+      { path: ':id/edit', component: RecipeFormComponent }
     ]
   },
-  { path: 'shopping-list', pathMatch: 'full', component: ShoppingListComponent },
   {
-    path: '',
-    redirectTo: '/recipes',
-    pathMatch: 'full'
+    path: 'shopping-list',
+    pathMatch: 'full',
+    component: ShoppingListComponent
   },
+  { path: '', pathMatch: 'full', component: LandingComponent }
+  // {
+  //   path: '',
+  //   redirectTo: '/recipes',
+  //   pathMatch: 'full'
+  // }
   // { path: '**', component: PageNotFoundComponent }
 ];
