e128bee67059592a4bf3ee0a523b94dce78ae934
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-25 19:03:04 -0400

    Map Login component and Signup component to its own routes

--- ui/src/app/routes/app.routes.ts
+++ ui/src/app/routes/app.routes.ts
@@ -4,6 +4,8 @@ import { ShoppingListComponent } from '../components/shopping-list/shopping-list
 import { RecipeDetailComponent } from '../components/recipes/recipe-detail/recipe-detail.component';
 import { RecipeFormComponent } from '../components/recipes/recipe-form/recipe-form.component';
 import { LandingComponent } from '../components/landing/landing.component';
+import { LoginComponent } from '../components/auth/login/login.component';
+import { SignupComponent } from '../components/auth/signup/signup.component';
 
 export const appRoutes: Routes = [
   {
@@ -20,6 +22,16 @@ export const appRoutes: Routes = [
     path: 'shopping-list',
     pathMatch: 'full',
     component: ShoppingListComponent
+  },
+  {
+    path: 'login',
+    pathMatch: 'full',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    pathMatch: 'full',
+    component: SignupComponent
   },
   { path: '', pathMatch: 'full', component: LandingComponent }
   // {
