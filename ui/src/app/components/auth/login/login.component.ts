cca94c8e4137ce3316a40a06ae2b7b7aa1ec7a03
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-26 16:43:43 -0400

    Subscribe to errors

--- ui/src/app/components/auth/login/login.component.ts
+++ ui/src/app/components/auth/login/login.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+  logInErrorsSubscription: Subscription;
+  errors;
+
   @ViewChild('form')
   shoppingListForm: NgForm;
   constructor(private authService: AuthService) {}
@@ -15,7 +19,17 @@ export class LoginComponent implements OnInit {
   ngOnInit() {}
 
   login(form: NgForm) {
+    this.logInErrorsSubscription = this.authService.loginErrors.subscribe(
+      errors => {
+        this.errors = errors;
+      }
+    );
+
     const { value } = form;
     this.authService.login(value);
+  }
+
+  ngOnDestroy() {
+    this.logInErrorsSubscription.unsubscribe();
   }
 }
