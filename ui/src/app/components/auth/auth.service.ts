395c1adc01b48e19ccd3efacd3e8b9db35ad6751
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-25 22:06:37 -0400

    Add methods to handle auth actions

--- ui/src/app/components/auth/auth.service.ts
+++ ui/src/app/components/auth/auth.service.ts
@@ -1,12 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { User } from './user.model';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  currentUser: User;
+  constructor(
+    private http: HttpClient,
+    public jwtHelper: JwtHelperService,
+    private router: Router
+  ) {}
+
   login(data) {
-    return this.http.post('/api/users/login', data).subscribe(data => {
-      console.log(data);
-    });
+    return this.http.post('/api/users/login', data).subscribe(
+      data => {
+        this.setAuthToken(data['token']);
+        this.setCurrentUser(data['token']);
+      },
+      err => console.log(err)
+    );
+  }
+
+  private setAuthToken(token) {
+    localStorage.setItem('jwtToken', token);
+  }
+
+  private setCurrentUser(token) {
+    const { id, name, avatar, exp } = this.jwtHelper.decodeToken(token);
+    this.currentUser = new User(id, name, avatar, exp);
+  }
+
+  getCurrentUser() {
+    return this.currentUser;
+  }
+
+  getAuthToken() {
+    return localStorage.getItem('jwtToken');
+  }
+
+  logout() {
+    localStorage.removeItem('jwtToken');
+    this.router.navigate(['/login']);
   }
 }
