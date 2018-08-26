2e66e4538024bb8c8d0d5c7459f08d5b1acfbdee
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-26 17:36:08 -0400

    Define form control refs and subscription to form errors

--- ui/src/app/components/auth/signup/signup.component.ts
+++ ui/src/app/components/auth/signup/signup.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
+  signUpForm: FormGroup;
+  signUpErrorsSubscription: Subscription;
+  errors;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {
+  onSubmit() {
+    this.authService.signup(this.signUpForm.value);
   }
 
+  private initForm() {
+    let name = '';
+    let email = '';
+    let password = '';
+    let password2 = '';
+
+    this.signUpForm = new FormGroup({
+      name: new FormControl(name, Validators.required),
+      email: new FormControl(email, Validators.required),
+      password: new FormControl(password, Validators.required),
+      password2: new FormControl(password2, Validators.required)
+    });
+  }
+
+  ngOnInit() {
+    this.signUpErrorsSubscription = this.authService.signUpErrors.subscribe(
+      errors => (this.errors = errors)
+    );
+
+    this.initForm();
+  }
+
+  ngOnDestroy() {
+    this.signUpErrorsSubscription.unsubscribe();
+  }
 }
