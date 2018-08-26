a77206ee57a4ec6c6996343ac228febb42512d7b
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-26 19:49:50 -0400

    Fetch recipes

--- ui/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ ui/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -20,7 +20,7 @@ export class RecipeListComponent implements OnInit, OnDestroy {
         this.recipes = recipes;
       }
     );
-    this.recipes = this.recipeService.getRecipes();
+    this.recipeService.getRecipes();
   }
 
   ngOnDestroy() {
