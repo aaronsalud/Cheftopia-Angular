f883905fe5276bccd144f25b54b9908f84dd28ab
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-25 20:44:10 -0400

    Add action to login user

--- ui/src/app/components/auth/login/login.component.ts
+++ ui/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('form')
+  shoppingListForm: NgForm;
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  login(form: NgForm) {
+    const { value } = form;
+    this.authService.login(value);
   }
-
 }
