c51118f2de2e8e7ffb50a96203602bd2ee8e6f7f
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-25 22:06:15 -0400

    Add AuthInterceptor

---
+++ ui/src/app/components/auth/auth.interceptor.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    // add authorization header with jwt token if available
+    const authToken = this.authService.getAuthToken();
+    const currentUser = this.authService.getCurrentUser();
+    if (currentUser && authToken) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: authToken
+        }
+      });
+    }
+
+    return next.handle(request);
+  }
+}
