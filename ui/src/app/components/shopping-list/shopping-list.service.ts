af82d2a8a1bd792f85cef7d71ea0acbdb9456da1
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-27 10:10:27 -0400

    Avoid error

--- ui/src/app/components/shopping-list/shopping-list.service.ts
+++ ui/src/app/components/shopping-list/shopping-list.service.ts
@@ -5,12 +5,12 @@ import { Subject } from 'rxjs';
 
 @Injectable()
 export class ShoppingListService {
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
   ingredientsChanged = new Subject<Ingredient[]>();
   ingredientEdit = new Subject<number>();
   ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
+    new Ingredient(1, 'Apples', 5),
+    new Ingredient(2, 'Tomatoes', 10)
   ];
 
   addIngredient(ingredient: Ingredient) {
