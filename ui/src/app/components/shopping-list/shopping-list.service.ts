40375a32cc86d838424581f67ae0bb28584dd9f9
Aaron Mikhail Salud <amsalud@quartermaster.house>
2018-08-27 16:30:28 -0400

    Add method to fetch shoppinglists

--- ui/src/app/components/shopping-list/shopping-list.service.ts
+++ ui/src/app/components/shopping-list/shopping-list.service.ts
@@ -2,16 +2,63 @@ import { Ingredient } from '../shared/ingredient.model';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ShoppingList } from './shopping-list.model';
 
 @Injectable()
 export class ShoppingListService {
-  constructor(private router: Router) { }
+  shoppinglistsUpdated = new Subject<ShoppingList[]>();
   ingredientsChanged = new Subject<Ingredient[]>();
   ingredientEdit = new Subject<number>();
+  shopping_lists: ShoppingList[];
+
   ingredients: Ingredient[] = [
     new Ingredient(1, 'Apples', 5),
     new Ingredient(2, 'Tomatoes', 10)
   ];
+
+  constructor(private router: Router, private http: HttpClient) { }
+
+  private generateIngredient(ingredient) {
+    return new Ingredient(ingredient.id, ingredient.name, ingredient.amount);
+  }
+
+  private generateIngredients(shoppinglistIngredients) {
+    const ingredients = [];
+    if (shoppinglistIngredients && shoppinglistIngredients.length > 0) {
+      shoppinglistIngredients.forEach(ingredient =>
+        ingredients.push(this.generateIngredient(ingredient))
+      );
+    }
+    return ingredients;
+  }
+
+  private generateShoppingList(shoppinglist, ingredients) {
+    return new ShoppingList(
+      shoppinglist.id,
+      shoppinglist.name,
+      shoppinglist.description,
+      shoppinglist.archived,
+      ingredients
+    );
+  }
+
+  // Fetch Shopping Lists Method
+  getShoppingLists() {
+    this.http.get('/api/shoppinglist').subscribe(
+      (shoppinglists: any) => {
+        this.shopping_lists = [];
+        shoppinglists.forEach((shoppinglist: any) => {
+          if (shoppinglist) {
+            const ingredients = this.generateIngredients(shoppinglist.ingredients);
+            this.shopping_lists.push(this.generateShoppingList(shoppinglist, ingredients));
+          }
+        });
+        this.shoppinglistsUpdated.next(this.shopping_lists.slice());
+      },
+      err => console.log(err)
+    );
+  }
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
