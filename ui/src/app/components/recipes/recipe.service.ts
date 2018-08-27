1b5019f8a8c81c34c4738ef7bab6896a825772d8
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-27 10:10:42 -0400

    Cleanup

--- ui/src/app/components/recipes/recipe.service.ts
+++ ui/src/app/components/recipes/recipe.service.ts
@@ -15,39 +15,23 @@ export class RecipeService {
     private shoppingListService: ShoppingListService,
     private http: HttpClient,
     private router: Router
-  ) {}
+  ) { }
 
   private recipes: Recipe[];
-
-  getRecipes() {
-    this.http.get('/api/recipe').subscribe(
-      (recipes: any) => {
-        this.recipes = [];
-        recipes.forEach((recipe: any) => {
-          if (recipe) {
-            const ingredients = [];
-            if (recipe.ingredients && recipe.ingredients.length > 0) {
-              recipe.ingredients.forEach(ingredient =>
-                ingredients.push(this.generateIngredient(ingredient))
-              );
-            }
-            this.recipes.push(this.generateRecipe(recipe, ingredients));
-            this.recipesUpdated.next(this.recipes.slice());
-          }
-        });
-      },
-      err => console.log(err)
-    );
-  }
-
-  setCurrentRoute(currentRoute: ActivatedRoute) {
-    this.route = currentRoute;
-  }
 
   private generateIngredient(ingredient) {
     return new Ingredient(ingredient.id, ingredient.name, ingredient.amount);
   }
 
+  private generateIngredients(recipeIngredients) {
+    const ingredients = [];
+    if (recipeIngredients && recipeIngredients.length > 0) {
+      recipeIngredients.forEach(ingredient =>
+        ingredients.push(this.generateIngredient(ingredient))
+      );
+    }
+    return ingredients;
+  }
   private generateRecipe(recipe, ingredients) {
     return new Recipe(
       recipe.id,
@@ -58,22 +42,39 @@ export class RecipeService {
     );
   }
 
+  setCurrentRoute(currentRoute: ActivatedRoute) {
+    this.route = currentRoute;
+  }
+
   getRecipeById(id: number) {
     return this.recipes && this.recipes.length > 0
       ? this.recipes.filter((recipe: Recipe) => recipe.id === id)[0]
       : null;
   }
 
+  // Fetch Recipes Method
+  getRecipes() {
+    this.http.get('/api/recipe').subscribe(
+      (recipes: any) => {
+        this.recipes = [];
+        recipes.forEach((recipe: any) => {
+          if (recipe) {
+            const ingredients = this.generateIngredients(recipe.ingredients);
+            this.recipes.push(this.generateRecipe(recipe, ingredients));
+            this.recipesUpdated.next(this.recipes.slice());
+          }
+        });
+      },
+      err => console.log(err)
+    );
+  }
+
+  // Create Recipe Method
   addRecipe(recipe: Recipe) {
     this.http.post('/api/recipe', recipe).subscribe(
       (recipe: any) => {
         if (recipe) {
-          const ingredients = [];
-          if (recipe.ingredients && recipe.ingredients.length > 0) {
-            recipe.ingredients.forEach(ingredient =>
-              ingredients.push(this.generateIngredient(ingredient))
-            );
-          }
+          const ingredients = this.generateIngredients(recipe.ingredients);
           this.recipes.push(this.generateRecipe(recipe, ingredients));
           this.recipesUpdated.next(this.recipes.slice());
           this.router.navigate(['../'], { relativeTo: this.route });
@@ -83,17 +84,13 @@ export class RecipeService {
     );
   }
 
+
+  // Update Recipe Method
   editRecipe(id: number, recipe: Recipe) {
     this.http.put(`/api/recipe/${id}`, recipe).subscribe(
       (recipe: any) => {
         if (recipe) {
-          const ingredients = [];
-          if (recipe.ingredients && recipe.ingredients.length > 0) {
-            recipe.ingredients.forEach(ingredient =>
-              ingredients.push(this.generateIngredient(ingredient))
-            );
-          }
-
+          const ingredients = this.generateIngredients(recipe.ingredients);
           const index = this.recipes.findIndex(
             recipeItem => recipeItem.id === recipe.id
           );
@@ -106,6 +103,7 @@ export class RecipeService {
     );
   }
 
+  // Delete Recipe Method
   deleteRecipe(id: number) {
     this.http.delete(`/api/recipe/${id}`).subscribe(
       () => {
