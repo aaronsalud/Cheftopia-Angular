f618315df30610a6b83c1c016037c664117c862f
Aaron Mikhail Salud <amsalud@quartermaster.house>
2018-08-27 17:23:45 -0400

    Add subscription to fetch event changes

--- ui/src/app/components/shopping-list/shopping-list.component.ts
+++ ui/src/app/components/shopping-list/shopping-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 import { Subscription } from 'rxjs';
+import { ShoppingList } from './shopping-list.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -9,21 +10,36 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
+  shoppinglists: ShoppingList[];
   ingredients: Ingredient[];
+  private shoppinglistsUpdatedSubscription: Subscription;
   private ingredientsChangedSubscription: Subscription;
-  constructor(private shoppingListService: ShoppingListService) {}
+  constructor(private shoppingListService: ShoppingListService) { }
 
   editItem(index: number) {
     this.shoppingListService.ingredientEdit.next(index);
   }
-  ngOnInit() {
-    this.ingredients = this.shoppingListService.getIngredients();
+
+  setupSubscriptions() {
+    this.shoppinglistsUpdatedSubscription = this.shoppingListService.shoppinglistsUpdated.subscribe(
+      (shoppinglists: ShoppingList[]) => {
+        this.shoppinglists = shoppinglists;
+        console.log(this.shoppinglists);
+      }
+    );
     this.ingredientsChangedSubscription = this.shoppingListService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[]) => (this.ingredients = ingredients)
     );
   }
 
+  ngOnInit() {
+    this.setupSubscriptions();
+    this.shoppingListService.getShoppingLists();
+    // this.ingredients = this.shoppingListService.getIngredients();
+  }
+
   ngOnDestroy() {
     this.ingredientsChangedSubscription.unsubscribe();
+    this.shoppinglistsUpdatedSubscription.unsubscribe();
   }
 }
