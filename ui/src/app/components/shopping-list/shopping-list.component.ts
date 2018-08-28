56e16b8b709d7c4ef7638422f0ccc20905f385b2
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-08-27 22:17:24 -0400

    Cleanup ShoppingListComponent

--- ui/src/app/components/shopping-list/shopping-list.component.ts
+++ ui/src/app/components/shopping-list/shopping-list.component.ts
@@ -10,23 +10,15 @@ import { ShoppingList } from './shopping-list.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  shoppinglists: ShoppingList[];
   ingredients: Ingredient[];
-  private shoppinglistsUpdatedSubscription: Subscription;
   private ingredientsChangedSubscription: Subscription;
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService) {}
 
   editItem(index: number) {
     this.shoppingListService.ingredientEdit.next(index);
   }
 
   setupSubscriptions() {
-    this.shoppinglistsUpdatedSubscription = this.shoppingListService.shoppinglistsUpdated.subscribe(
-      (shoppinglists: ShoppingList[]) => {
-        this.shoppinglists = shoppinglists;
-        console.log(this.shoppinglists);
-      }
-    );
     this.ingredientsChangedSubscription = this.shoppingListService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[]) => (this.ingredients = ingredients)
     );
@@ -35,11 +27,9 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.setupSubscriptions();
     this.shoppingListService.getShoppingLists();
-    // this.ingredients = this.shoppingListService.getIngredients();
   }
 
   ngOnDestroy() {
     this.ingredientsChangedSubscription.unsubscribe();
-    this.shoppinglistsUpdatedSubscription.unsubscribe();
   }
 }
