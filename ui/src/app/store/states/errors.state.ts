e55af08dc6ff94b80f358b6a972ec9e49b5b3ff7
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-02 17:51:31 -0400

    Fix state default and state setting

--- ui/src/app/store/states/errors.state.ts
+++ ui/src/app/store/states/errors.state.ts
@@ -2,15 +2,13 @@ import { State, Action } from '@ngxs/store';
 import { GetErrors, ClearErrors } from '../actions/errors.actions';
 @State({
   name: 'errors',
-  defaults: {
-    errors: {}
-  }
+  defaults: {}
 })
 export class ErrorsState {
   @Action(GetErrors)
-  getErrors({ patchState }: any, { payload }: GetErrors) {
-    patchState({
-      errors: payload
+  getErrors({ setState }: any, { payload }: GetErrors) {
+    setState({
+      ...payload
     });
   }
 
