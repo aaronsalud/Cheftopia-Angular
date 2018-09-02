491ff7b14f7224fa819c5899e20e6667fc6a61c5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-02 17:54:59 -0400

    Map login component to state action method

--- ui/src/app/components/auth/login/login.component.ts
+++ ui/src/app/components/auth/login/login.component.ts
@@ -1,35 +1,36 @@
 import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
-import { Subscription } from 'rxjs';
+import { Store } from '@ngxs/store';
+import { LoginUser } from '../../../store/actions/auth.actions';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit, OnDestroy {
-  logInErrorsSubscription: Subscription;
+export class LoginComponent implements OnInit {
   errors;
 
   @ViewChild('form')
   shoppingListForm: NgForm;
-  constructor(private authService: AuthService) {}
-
-  ngOnInit() {
-    this.logInErrorsSubscription = this.authService.loginErrors.subscribe(
-      errors => {
-        this.errors = errors;
-      }
-    );
+  constructor(
+    private authService: AuthService,
+    private store: Store,
+    private router: Router
+  ) {
+    this.store
+      .select(state => state.errors)
+      .subscribe(errors => (this.errors = errors));
   }
+
+  ngOnInit() {}
 
   login(form: NgForm) {
     const { value } = form;
-    this.authService.login(value);
-  }
-
-  ngOnDestroy() {
-    this.logInErrorsSubscription.unsubscribe();
+    this.store.dispatch(
+      new LoginUser(value, this.authService, this.store, this.router)
+    );
   }
 }
