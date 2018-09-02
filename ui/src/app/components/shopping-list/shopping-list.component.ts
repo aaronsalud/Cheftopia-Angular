9ff22d2c3560f76a38b9bf3ea24365251835e9a0
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-02 18:40:48 -0400

    Cleanup shopping list component

--- ui/src/app/components/shopping-list/shopping-list.component.ts
+++ ui/src/app/components/shopping-list/shopping-list.component.ts
@@ -1,34 +1,11 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from './shopping-list.service';
-import { Subscription } from 'rxjs';
-import { ShoppingList } from './shopping-list.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[];
-  private ingredientsChangedSubscription: Subscription;
-  constructor(private shoppingListService: ShoppingListService) {}
-
-  editItem(index: number) {
-    this.shoppingListService.ingredientEdit.next(index);
-  }
-
-  setupSubscriptions() {
-    this.ingredientsChangedSubscription = this.shoppingListService.ingredientsChanged.subscribe(
-      (ingredients: Ingredient[]) => (this.ingredients = ingredients)
-    );
-  }
-
-  ngOnInit() {
-    this.setupSubscriptions();
-  }
-
-  ngOnDestroy() {
-    this.ingredientsChangedSubscription.unsubscribe();
-  }
+export class ShoppingListComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {}
 }
