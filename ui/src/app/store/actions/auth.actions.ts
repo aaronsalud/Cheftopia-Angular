661aeee079faecf591391e03448499fe62b7169a
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-02 17:52:09 -0400

    Add LoginUser and setCurrentUser action methods

--- ui/src/app/store/actions/auth.actions.ts
+++ ui/src/app/store/actions/auth.actions.ts
@@ -2,6 +2,7 @@ import { AuthService } from '../../components/auth/auth.service';
 import { Router } from '@angular/router';
 import { Store } from '@ngxs/store';
 import { GetErrors, ClearErrors } from './errors.actions';
+import { User } from '../../components/auth/user.model';
 
 // Register User Action
 export class RegisterUser {
@@ -24,33 +25,38 @@ export class RegisterUser {
 }
 
 // Login User Action
-// export class LoginUser {
-//   static readonly type = '[AUTH] Login User';
+export class LoginUser {
+  static readonly type = '[AUTH] Login User';
+  payload: any;
 
-//   constructor(
-//     payload: any,
-//     private authService: AuthService,
-//     private store: Store,
-//     private router: Router
-//   ) {
-//     this.store.dispatch(new ClearErrors());
-//     this.authService.signup(payload).subscribe(
-//       () => {
-//         this.router.navigate(['/login']);
-//       },
-//       err => this.store.dispatch(new GetErrors(err))
-//     );
-//   }
-// }
+  constructor(
+    payload: any,
+    private authService: AuthService,
+    private store: Store,
+    private router: Router
+  ) {
+    this.store.dispatch(new ClearErrors());
+    this.authService.login(payload).subscribe(
+      data => {
+        this.authService.setAuthToken(data['token']);
+        this.router.navigate(['/recipes']);
+        this.store.dispatch(
+          new SetCurrentUser(true, this.authService.getCurrentUser())
+        );
+      },
+      err => this.store.dispatch(new GetErrors(err))
+    );
+  }
+}
 
-// export class getShoppingLists {
-//   static readonly type = '[SHOPPING LIST] Get';
-
-//   constructor() {}
-// }
-
-// export class RemoveTutorial {
-//     static readonly type = '[TUTORIAL] Remove'
-
-//     constructor(public payload: string) {}
-// }
+// Set Current User Action
+export class SetCurrentUser {
+  static readonly type = '[AUTH] Set Current User';
+  payload: any;
+  constructor(isAuthenticated: boolean, user: User) {
+    this.payload = {
+      isAuthenticated,
+      user
+    };
+  }
+}
