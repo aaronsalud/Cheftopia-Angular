eebbcacebeaed02101c024daecbcf1e8fa9c7a8e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-02 21:35:05 -0400

    Add ShoppingList state action methods

---
+++ ui/src/app/store/actions/shopping-list.actions.ts
@@ -0,0 +1,46 @@
+import { ShoppingListService } from '../../components/shopping-list/shopping-list.service';
+import { Store } from '@ngxs/store';
+import { SetErrors } from './errors.actions';
+import { ShoppingList } from '../../components/shopping-list/shopping-list.model';
+
+// Fetch Shopping List Data
+export class GetShoppingLists {
+  static readonly type = '[SHOPPING LIST] Fetch shopping lists';
+  private shoppinglists: ShoppingList[] = [];
+
+  constructor(
+    queryParam: string = null,
+    private shoppinglistService: ShoppingListService,
+    private store: Store
+  ) {
+    this.store.dispatch(new ShoppingListLoading());
+    this.shoppinglistService.getShoppingLists(queryParam).subscribe(
+      (shoppinglists: any) => {
+        shoppinglists.forEach((shoppinglist: any) => {
+          if (shoppinglist) {
+            this.shoppinglists.push(
+              this.shoppinglistService.generateShoppingList(shoppinglist)
+            );
+          }
+        });
+        this.store.dispatch(new SetShoppingLists(shoppinglists));
+      },
+      err => {
+        this.store.dispatch(new ShoppingListLoading());
+        this.store.dispatch(new SetErrors(err));
+      }
+    );
+  }
+}
+
+// Set Shopping Lists State
+export class SetShoppingLists {
+  static readonly type = '[SHOPPING LIST] Set Shopping Lists';
+  constructor(public payload: ShoppingList[]) {}
+}
+
+// Set Loading State
+export class ShoppingListLoading {
+  static readonly type = '[SHOPPING LIST] Loading';
+  constructor() {}
+}
