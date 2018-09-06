b0353a160acda7e922e6dce37d7442ec06f76695
Aaron Mikhail Salud <amsalud@quartermaster.house>
2018-09-06 14:30:31 -0400

    Cleanup

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 
-// import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './components/auth/interceptors/auth.interceptor';
 import { HttpErrorInterceptor } from './components/auth/interceptors/http-error.interceptor';
 
