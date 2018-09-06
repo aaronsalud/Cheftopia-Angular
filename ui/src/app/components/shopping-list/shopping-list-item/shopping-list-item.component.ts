822e6b876315dda22983ed13688e6b0f2ca71fe5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-05 22:09:34 -0400

    Dispatch delete shopping list action

--- ui/src/app/components/shopping-list/shopping-list-item/shopping-list-item.component.ts
+++ ui/src/app/components/shopping-list/shopping-list-item/shopping-list-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ShoppingList } from '../shopping-list.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { Store } from '@ngxs/store';
+import { DeleteShoppingList } from '../../../store/actions/shopping-list.actions';
 
 @Component({
   selector: 'app-shopping-list-item',
@@ -12,10 +14,15 @@ export class ShoppingListItemComponent implements OnInit {
   shoppinglist: ShoppingList;
   @Input()
   hideActions: boolean;
-  constructor(private shoppingListService: ShoppingListService) {}
+  constructor(
+    private shoppingListService: ShoppingListService,
+    private store: Store
+  ) {}
 
   deleteItem(id) {
-    this.shoppingListService.deleteShoppingList(id);
+    this.store.dispatch(
+      new DeleteShoppingList(id, this.store, this.shoppingListService)
+    );
   }
 
   ngOnInit() {}
