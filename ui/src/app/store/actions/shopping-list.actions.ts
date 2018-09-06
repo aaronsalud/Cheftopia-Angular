1cc8b7275aa6fa1eb9e720aafa10c7f01c9462c5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-05 22:09:47 -0400

    Add action method to delete a shopping list

--- ui/src/app/store/actions/shopping-list.actions.ts
+++ ui/src/app/store/actions/shopping-list.actions.ts
@@ -33,6 +33,39 @@ export class GetShoppingLists {
   }
 }
 
+// Delete a Shopping List
+export class DeleteShoppingList {
+  static readonly type = '[SHOPPING LIST] Delete a Shopping list';
+  constructor(
+    id: number,
+    private store: Store,
+    private shoppinglistService: ShoppingListService
+  ) {
+    this.store.dispatch(new ShoppingListLoading());
+    const subscription = this.store
+      .select(state => state.shoppinglistDashboard)
+      .subscribe(({ shoppinglists }) => {
+        if (shoppinglists) {
+          this.shoppinglistService.deleteShoppingList(id).subscribe(
+            () => {
+              const index = shoppinglists.findIndex(
+                shoppinglist => shoppinglist.id === id
+              );
+              shoppinglists.splice(index, 1);
+              subscription.unsubscribe();
+              this.store.dispatch(new SetShoppingLists(shoppinglists));
+            },
+            err => {
+              subscription.unsubscribe();
+              this.store.dispatch(new ShoppingListLoading());
+              this.store.dispatch(new SetErrors(err));
+            }
+          );
+        }
+      });
+  }
+}
+
 // Fetch Shopping List Data
 export class GetShoppingList {
   static readonly type = '[SHOPPING LIST] Fetch shopping list by Id';
