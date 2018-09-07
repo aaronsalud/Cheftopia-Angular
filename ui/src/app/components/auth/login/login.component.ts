a23883b6399c839b5bf31fa7f22845294a780666
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-06 22:04:23 -0400

    Add unsubscribe statement to errors state subscription

--- ui/src/app/components/auth/login/login.component.ts
+++ ui/src/app/components/auth/login/login.component.ts
@@ -1,30 +1,34 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { Store } from '@ngxs/store';
-import { LoginUser } from '../../../store/actions/auth.actions';
-import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   errors;
+  storeSubscription: Subscription;
 
   @ViewChild('form')
   loginForm: NgForm;
   constructor(private authService: AuthService, private store: Store) {
-    this.store
+    this.storeSubscription = this.store
       .select(state => state.errors)
       .subscribe(errors => (this.errors = errors));
   }
 
-  ngOnInit() {}
-
   login(form: NgForm) {
     const { value } = form;
     this.authService.login(value);
+  }
+
+  ngOnInit() {}
+
+  ngOnDestroy() {
+    this.storeSubscription.unsubscribe();
   }
 }
