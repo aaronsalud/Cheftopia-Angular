244e044587c9bf4807a1100c6094c72715971b7b
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-09 19:02:25 -0400

    Use service subscription instead of store

--- ui/src/app/components/shopping-list/shopping-list.component.ts
+++ ui/src/app/components/shopping-list/shopping-list.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngxs/store';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
+  shoppingListLoadingSuscription: Subscription;
   loading: boolean = false;
-  constructor(private store: Store) {
-    this.store
-      .select(state => state.shoppinglistDashboard)
-      .subscribe(({ loading }) => {
+  constructor(private shoppinglistService: ShoppingListService) {}
+  ngOnInit() {
+    this.shoppingListLoadingSuscription = this.shoppinglistService.shoppingListLoading.subscribe(
+      loading => {
         this.loading = loading;
-      });
+      }
+    );
   }
-  ngOnInit() {}
+
+  ngOnDestroy() {
+    this.shoppingListLoadingSuscription.unsubscribe();
+  }
 }
