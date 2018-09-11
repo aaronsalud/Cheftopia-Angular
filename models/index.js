09827c7c93c146a839a12d7623de14fe349c4932
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-11 13:11:16 -0400

    Fix db config

--- models/index.js
+++ models/index.js
@@ -1,18 +1,27 @@
 'use strict';
 
-var fs        = require('fs');
-var path      = require('path');
+var fs = require('fs');
+var path = require('path');
 var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+var basename = path.basename(__filename);
+var env = process.env.NODE_ENV || 'development';
+var config = require('../config/keys').postgresURI;
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+var db = {};
+
+// Connect to PostgresSQL
+var sequelize = new Sequelize(config, {
+  dialect: 'postgres',
+  operatorsAliases: false
+});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('PostgresSQL DB Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 fs
   .readdirSync(__dirname)
