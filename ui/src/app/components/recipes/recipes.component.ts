6653b1da26820a55085491108899257f61b5bf58
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-13 23:13:39 -0400

    Add subscription to recipe loading

--- ui/src/app/components/recipes/recipes.component.ts
+++ ui/src/app/components/recipes/recipes.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { RecipeService } from './recipe.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
-export class RecipesComponent implements OnInit {
-  constructor() {}
+export class RecipesComponent implements OnInit, OnDestroy {
+  recipeLoadingSuscription: Subscription;
+  loading: boolean = true;
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.recipeLoadingSuscription = this.recipeService.recipeLoading.subscribe(
+      loading => {
+        this.loading = loading;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.recipeLoadingSuscription.unsubscribe();
+  }
 }
